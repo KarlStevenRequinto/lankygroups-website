3790ca51d5a80aafd77e6b2fcc37d87b44b3cba6
KarlStevenRequinto <lerdrobott@gmail.com>
2024-08-25 21:53:19 +0800

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 function App() {
     return (
         <>
             <NavBar />
+            <Home/>
         </>
     );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import logo from "../assets/logo.png";
 import { Link } from "react-scroll";
-import { FaXmark, FaBars} from "react-icons/fa6";
+import { FaXmark, FaBars } from "react-icons/fa6";
 const NavBar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isSticky, setIsSticky] = useState(false);
@@ -31,7 +31,7 @@ const NavBar = () => {
     ];
     return (
         <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
-            <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 left-0 right-0 border-b bg-white duration-300":""}` }>
+            <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 left-0 right-0 border-b bg-white duration-300" : ""}`}>
                 <div className="flex justify-between items-center text-base gap-8">
                     <a href="" className="text-2xl font-semibold flex items-center space-x-3">
                         <img src={logo} alt="logo" className="w-10 inline-block items-center" />
@@ -39,22 +39,48 @@ const NavBar = () => {
                     </a>
                     <ul className="md:flex space-x-12 hidden">
                         {navItems.map(({ link, path }) => (
-                            <Link key={path} to={path} spy={true} smooth={true} offset={-100} className="block text-base text-gray900 hover:text-brandPrimary first:font-medium">
+                            <Link
+                                key={path}
+                                to={path}
+                                spy={true}
+                                smooth={true}
+                                offset={-100}
+                                className="block text-base text-gray900 hover:text-brandPrimary first:font-medium"
+                            >
                                 {link}
                             </Link>
                         ))}
                     </ul>
 
                     <div className="space-x-12 hidden lg:flex items-center">
-                        <a href="/" className=" hidden lg:flex items-center hover:text-gray-900">Login</a>
-                        <button className="bg-brandPrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-neutralDGrey">Sign up</button>
+                        <a href="/" className=" hidden lg:flex items-center hover:text-gray-900">
+                            Login
+                        </a>
+                        <button className="bg-brandPrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-neutralDGrey">
+                            Sign up
+                        </button>
                     </div>
 
                     <div className="md:hidden">
                         <button className="focus:outline-none text-neutralDGrey focus:text-gray-500" onClick={toggleMenu}>
-                            {isMenuOpen ? <FaXmark className="h-6 w-6 text-neutralDGrey"/> : <FaBars className="h-6 w-6 text-neutralDGrey"/>}
+                            {isMenuOpen ? <FaXmark className="h-6 w-6 text-neutralDGrey" /> : <FaBars className="h-6 w-6 text-neutralDGrey" />}
                         </button>
                     </div>
+                </div>
+
+                <div className={`space-y-4 px-4 mt-16 py-7 bg-brandPrimary ${isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"}`}>
+                    {navItems.map(({ link, path }) => (
+                        <Link
+                            to={path}
+                            spy={true}
+                            smooth={true}
+                            offset={-100}
+                            key={path}
+                            className="block text-base text-white hover:text-neutralDGrey first:font-medium"
+                        >
+                            {link}
+                        </Link>
+                    ))}
                 </div>
             </nav>
         </header>
