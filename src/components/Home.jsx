014db89c889c80eb0540a581a179dae1e3b77416
KarlStevenRequinto <lerdrobott@gmail.com>
2024-08-25 22:22:34 +0800

    Adding images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Carousel } from "flowbite-react";
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+            <Carousel className="w-full mx-auto">
+                <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+                    <div>
+                        <img src="" alt="image"/>
+                    </div>
+                </div>
+                <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">Slide 2</div>
+                <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">Slide 3</div>
+            </Carousel>
+        </div>
+    );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind")
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -16,6 +20,8 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 }
 
