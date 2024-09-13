b3d9d51251ab0285619a7ff0c0beaa7a5c34ce37
KarlStevenRequinto <lerdrobott@gmail.com>
2024-09-13 16:45:48 +0800

    about section and services section update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,18 +3,18 @@ import banner2 from "../assets/banner2.png";
 
 const About = () => {
     return (
-        <div >
+        <div>
             <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-8" id="about">
                 <div className="md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12">
                     <div>
                         <img src={banner2} alt="image" />
                     </div>
                     <div className="md:w-3/5 mx-auto">
-                        <h2 className="text-4xl text-neutralDGrey font-semibold mb-4 md:w-4/5">The unseen of spending three years at Pixelgrade</h2>
+                        <h2 className="text-4xl text-neutralDGrey font-semibold mb-4 md:w-4/5">Redefining Premier Remote Support</h2>
                         <p className="md:w-3/4 text-sm text-neutralGrey mb-8">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius
-                            fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet
-                            risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.
+                            Lanky Nation Groups was founded with a mission to provide world-class virtual assistants and executive assistants to
+                            businesses around the world. Our team consists of experienced professionals, all based in the Philippines, who bring their
+                            passion and dedication to every task.
                         </p>
                         <button className="btn-primary">Learn More</button>
                     </div>
@@ -23,11 +23,11 @@ const About = () => {
 
             <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto bg-neutralSilver py-16">
                 <div className="flex flex-col md:flex-row justify-between items-center gap-8">
-                    <div className="md:w-1/2">
+                <div className="md:w-1/2">
                         <h2 className="text-4xl text-neutralDGrey font-semibold mb-4 md:w-2/3">
-                            Helping a local <span className="text-brandPrimary">business reinvent itself</span>
+                            A Vision to <span className="text-brandPrimary">Transform Lives and Businesses</span>
                         </h2>
-                        <p className="md:w-3/4 text-sm text-neutralGrey mb-8">We reached here with our hard work and dedication</p>
+                        <p className="md:w-3/4 text-sm text-neutralGrey mb-8">Our mission is to uplift Filipino professionals while helping businesses worldwide grow with world-class support.</p>
                     </div>
 
                     <div className="md:w-1/2 mx-auto flex sm:flex-row flex-col sm:items-center justify-around gap-12">
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -40,12 +40,12 @@ const Services = () => {
         },
     ];
     return (
-        <div className="md:px-14 py-16 max-w-screen-2xl mx-auto" id="service">
-            <div className="text-center my-8">
+        <div className="md:px-14 max-w-screen-2xl mx-auto" id="service">
+            <div className="text-center">
                 {/* <h2 className="text-4xl text-neutralDGrey font-semibold mb-2">Our Clients</h2>
-                <p className="text-neutralGrey">We have been working with some Fortune 500+ clients</p> */}
+                <p className="text-neutralGrey">We have been working with some Fortune 500+ clients</p>
 
-                {/* <div>images here</div> */}
+                <div>images here</div> */}
 
                 <div className="mt-20 md:w-3/4 mx-auto text-center bg-slate-200">
                     <h2 className="text-4xl text-neutralDGrey font-semibold mb-3">Efficient, Reliable, and Professional</h2>
@@ -54,7 +54,7 @@ const Services = () => {
 
                 <div className="mt-14 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:w-11/12 mx-auto gap-12">
                     {services.map((service) => (
-                        <div key={service.id} className="px-4 py-8 text-center md:w-[300px] mx-auto md:h-80 rounded-md shadow cursor-pointer hover:-translate-y-5 hover:border-b-4 hover:border-indigo-700 transition-all duration-300 flex items-center justify-center h-full ">
+                        <div key={service.id} className="px-4 py-8 text-center md:w-[300px] mx-auto md:h-80 rounded-md shadow hover:-translate-y-5 hover:border-b-4 hover:border-indigo-700 transition-all duration-300 flex items-center justify-center h-full ">
                             <div>
                                 <div className="bg-[#E8F5E9] h-14 w-14 mb-4 mx-auto rounded-tl-3xl rounded-br-3xl">
                                     <img src={service.image} alt="image" className="-ml-5"/>
